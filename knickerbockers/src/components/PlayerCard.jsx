b7e8a975197a32a6dc99ddb3967c5fc4f3859522
Dylan Ross <dylanross@Dylans-MacBook-Pro.local>
2021-03-22 12:49:39 -0400

    css

--- knickerbockers/src/components/PlayerCard.jsx
+++ knickerbockers/src/components/PlayerCard.jsx
@@ -23,15 +23,10 @@ function PlayerCard(props) {
   }
   apgs();
 
-  // const player = props.teams.find((player) => player.id === params.id);
-  // if (!player) {
-  //   return <h2></h2>;
-  // }
 
   return (
     <div className="players">
       <div className="player-card">
-        {/* <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.istockphoto.com%2Fphotos%2Fnerd-basketball-player-in-blue-white-picture-id160491538%3Fk%3D6%26m%3D160491538%26s%3D612x612%26w%3D0%26h%3DBydtaRNlL0q51LH5gsgNfoh3NCdj6aKnwPDu6M6CljM%3D&f=1&nofb=1" alt="Player Picture" width="300px" height="300px"/> */}
         <h2 className="stats" id="title">
           STATS
         </h2>
